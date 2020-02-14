cfb0f06e4ec09827d06bd723c66124c485545149
Nathan Keller <keller.nathan@gmail.com>
2020-02-14 10:59:44 -0700

    Test created for user story 3

---
+++ spec/features/movie/show_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+
+RSpec.describe Actor, type: :feature do
+  describe "actor show page" do
+    it "can see the actors information" do
+      studio_1 = Studio.create(name: "Theee Studio")
+
+      movie_1 = Movie.create(name: "Backlash", studio: studio_1)
+
+      actor_1 = Actor.create(name: "Bruce Willis", age: 60)
+      actor_2 = Actor.create(name: "J-Law", age: 20)
+
+      movie_1.actors << [actor_1, actor_2]
+
+      visit "/movies/#{movie_1.id}"
+
+      expect(page).to have_content(movie_1.name)
+      expect(page).to have_content(movie_1.year)
+      expect(page).to have_content(movie_1.genre)
+      expect(page).to have_content(actor_1.name)
+      expect(page).to have_content(actor_2.name)
+      expect(page).to have_content("Averge Actor Age: 40")
+    end
+  end
+end
+
+
+
+# Story 3
+# As a user,
+# When I visit a movie's show page.
+# I see the movie's name, creation year, and genre,
+# and a list of all its actors from youngest to oldest.
+# And I see the average age of all of the movie's actors
