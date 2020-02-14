d3aec4a67f33734c611c6addccbd849b34d9b350
Nathan Keller <keller.nathan@gmail.com>
2020-02-14 10:48:35 -0700

    Finally got the many to many under control

--- spec/models/actor_movie_spec.rb
+++ spec/models/actor_movie_spec.rb
@@ -1,4 +1,6 @@
-RSpec.describe ActorMovies, type: :model do
+require 'rails_helper'
+
+RSpec.describe ActorMovie, type: :model do
   describe "relationships" do
     it {should belong_to :movie}
     it {should belong_to :actor}
