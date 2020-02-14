96472c3fd327b894741df490e9ef59a866647a82
Nathan Keller <keller.nathan@gmail.com>
2020-02-14 11:22:25 -0700

    Test written for us4

--- spec/features/movie/show_spec.rb
+++ spec/features/movie/show_spec.rb
@@ -23,5 +23,37 @@ RSpec.describe Actor, type: :feature do
 
       expect(page).to have_content("Average Actor Age: 40")
     end
+
+    it "can fill in a form for an actors name" do
+      studio_1 = Studio.create(name: "Theee Studio")
+
+      movie_1 = Movie.create(name: "Backlash", genre: "action", year: 2019, studio: studio_1)
+
+      actor_1 = Actor.create(name: "Bruce Willis", age: 60)
+      actor_2 = Actor.create(name: "J-Law", age: 20)
+
+      movie_1.actors << [actor_1]
+
+      visit "/movies/#{movie_1.id}"
+      expect(page).to have_content(actor_1.name)
+      expect(page).to_not have_content(actor_2.name)
+
+      fill_in :name, with: actor_2.name
+
+      expect(current_path).to eq("/movies/#{movie_1.id}")
+      expect(page).to have_content(actor_2.name)
+
+    end
   end
 end
+
+
+
+# Story 4
+# As a user,
+# When I visit a movie show page,
+# I see a form for an actors name
+# and when I fill in the form with an existing actor's name
+# I am redirected back to that movie's show page
+# And I see the actor's name listed
+# (This should not break story 3, You do not have to test for a sad path)
